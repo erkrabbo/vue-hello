09805ba5e7e9778dc0478857b373e02ca3a5e742
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-10 17:54:22 +0100

    snack 2 (console log)

--- Snack/script.js
+++ Snack/script.js
@@ -2,6 +2,20 @@ const array = ['pippo', 'PLUTO', 'Paperino'];
 
 const newArray = array.map(element => element = element[0].toUpperCase() + element.slice(1).toLowerCase());
 
-console.log('snack 1', newArray)
+console.log('snack 1')
 console.log('partenza:', array)
 console.log('fine:', newArray)
+
+
+const animals =[
+    { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+    { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+    { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+
+const mammiferi = animals.filter(element => element.classe == 'mammiferi');
+console.log('----------------------')
+console.log('snack 2')
+console.log('partenza:', animals)
+console.log('fine', mammiferi);
+console.log('----------------------')
